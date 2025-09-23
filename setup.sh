f687ef7f8494a0bff48adaedb7a23990b0e7a4d3
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-09-23 14:50:44 +0200

    Also need the public_key scope

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ ssh-add ${sshkeypath}
 
 # authenticate gh on the web
 echo 'Authenticate the github CLI, press no when asked to upload ssh keys, we will do it manually'
-gh auth login -h github.com -s admin:ssh_signing_key
+gh auth login -h github.com -s admin:ssh_signing_key,admin:public_key
 
 read -p 'What do you want the title of your github ssh key to be? (e.g. Laptop, Desktop, etc.): ' keytitle
 gh ssh-key add "$sshkeypath" -t "$keytitle - auth" --type authentication 
